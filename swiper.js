a9649f03097dd20bae925be6982ad35cc13b7952
Mostafa <mostafa@MacBook-Air-von-Mostafa.fritz.box>
2026-09-19 10:15:51 +0200

    swiper function and first prototype

--- swiper.js
+++ swiper.js
@@ -1,4 +1,20 @@
 const swiper = new Swiper(".swiper", {
-
+    slidesPerView: "auto", 
+    spaceBetween: 22,
+    slideToClickedSlide: true, 
+    centeredSlides: true,
 }
 );
+
+swiper.on("click", function(){
+
+    const swiperEl = document.querySelector(".swiper"); 
+    if(swiper.clickedIndex === swiper.activeIndex && swiperEl.classList.contains("is-magazine"))
+    {
+        swiperEl.classList.remove("is-magazine");
+    }
+    else 
+    {
+        swiperEl.classList.add("is-magazine");
+    }
+});
