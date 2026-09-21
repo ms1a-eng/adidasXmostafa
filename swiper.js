18f789366e781ebe50d68dd069650dca74700f9f
Mostafa <mostafa@MacBook-Air-von-Mostafa.fritz.box>
2026-09-21 16:43:41 +0200

    added main page, hero dvision and some adjusment

---
+++ swiper.js
@@ -0,0 +1,48 @@
+const swiper = new Swiper(".swiper", {
+    slidesPerView: "auto",
+    spaceBetween: 8, 
+    breakpoints: {
+        768:{
+            spaceBetween: 14
+        },
+        1024:
+        {
+            spaceBetween:21
+        }
+    },
+    slideToClickedSlide: true, 
+    centeredSlides: false,
+}
+);
+
+
+swiper.on("click", function(){
+
+    const clicked = swiper.clickedSlide; 
+    if(!clicked) return;
+    const swiperEl = document.querySelector(".swiper");
+    const isAlreadyActive = clicked.classList.contains("is-active");
+    for(const card of document.querySelectorAll(".card"))
+    {
+        card.classList.remove("is-active");
+    }
+
+    if(isAlreadyActive)
+    {
+        swiperEl.classList.remove("is-magazine");
+    }
+    
+    else
+    {
+        clicked.classList.add("is-active");
+        swiperEl.classList.add("is-magazine");
+    }
+});
+
+const toggle = document.querySelector(".nav-switch-toggle");
+const navEl = document.querySelector(".nav-menu");
+
+toggle.addEventListener("click", function()
+{
+    navEl.classList.toggle("is-open");
+});
