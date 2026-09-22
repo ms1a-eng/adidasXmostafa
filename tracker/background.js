c61c3be3a15920c60446a53a96edca388b237fb4
Mostafa <mostafa@MacBook-Air-von-Mostafa.fritz.box>
2026-09-22 19:43:14 +0200

    added backend too into background.js

--- tracker/background.js
+++ tracker/background.js
@@ -27,7 +27,7 @@ chrome.alarms.onAlarm.addListener(async function(alarms){
                     {
                         console.log(`current tab check: ${currenttab["url"]}\n site is: ${site} `);
                         fetch(
-                            "http://https://mostafa.herokuapp.com/api/save" ,{
+                            "https://mostafa.herokuapp.com/api/save" ,{
                                 "method": "POST", 
                                 "headers": {
                                     "Content-Type": "application/json",
